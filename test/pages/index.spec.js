ecc87bd9972d27d7502e643c69a034e2f3696a77
ikuo saito <ikuosaito1989@gmail.com>
2020-08-26 10:28:34 +0900

    テストを追加

--- test/pages/index.spec.js
+++ test/pages/index.spec.js
@@ -2,8 +2,15 @@ import { mount } from '@vue/test-utils'
 import Index from '@/pages/index.vue'
 
 describe('Index', () => {
+  const wrapper = mount(Index)
+
   test('is a Vue instance', () => {
-    const wrapper = mount(Index)
     expect(wrapper.vm).toBeTruthy()
+  })
+
+  describe('computed', () => {
+    test('`title`が取得できること', () => {
+      expect(wrapper.vm.title).toBe('sample')
+    })
   })
 })
